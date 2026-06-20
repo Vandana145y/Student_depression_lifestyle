c180fb4f50b75b7068089852d083dabef3c911c9
Vandana145y <paripativandana@gmail.com>
2026-06-21 03:55:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 
 st.set_page_config(
-    page_title="Student Depression Predictor",
+    page_title="Student Depression Lifestyle Predictor",
     page_icon="🎓",
     layout="centered",
 )
